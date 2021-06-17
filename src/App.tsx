3623ea44e03ed7a9acce9ecca32143f30793441a
owenburton <owen.burton13@gmail.com>
2021-06-16 20:44:57 -0600

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>
+        Info retrieved from smart contract:
+      </h1>
+    </div>
+  );
+}
+
+export default App;
